9e9be569eea3064994aedb347f30060a86a9631e
rocoriyan <rcorrigan.other+github@gmail.com>
2024-02-05 14:48:40 +0000

    enhancement: get 'awesome' example working

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express"); //es5. import is es6
 
 const app = express();
 
-app.get("/health", (req, res) => {
+/*app.get("/health", (req, res) => {
   res.send("healthy");
-});
+});*/
+
+//          ↓ site path               ↓ directory?
+app.use("/example", express.static("example")); //'/example' and 'example' could be different. dont have to be the same
+
+app.use("/rocoriyan", express.static("other"));
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
