3290971850eb8a498f8ee46349e7408a4557f047
rocoriyan <rcorrigan.other+github@gmail.com>
2024-02-07 14:05:18 +0000

    cleanup for mongoose

--- src/server.js
+++ src/server.js
@@ -13,47 +13,18 @@ app.use(express.json());
 
 // HTTP Verb GET
 app.get("/books", (request, response) => {
-  console.log("/books: ", request.path);
-  response.send({ message:"success", fakeArr: fakeArr });
 });
 
 app.get("/books/getfirstbook", (request, response) => {
-  console.log("/books/books: ", request.path);
-  const book = fakeArr[0];
-  response.send({message: "success", book: book});
 });
 
 app.post("/books", (request, response) => {
-  console.log("title: ", request.body.title);
-  console.log("genre: ", request.body.genre);
-  console.log("author: ", request.body.author);
-
-  fakeArr.push(request.body);
-
-  /*let output;
-  for (let i = 0; fakeArr.length; i++){
-    if(fakeArr[i].title === request.body.title){
-      output = "theyre the same";
-    }
-  }
-  console.log(output);*/
-  response.send({message: "success", newBook: fakeArr[fakeArr.length - 1]});
 });
 
 app.put("/books", (request, response) => {
-  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
-  // change (update) the author to an new name
 });
 
 app.delete("/books", (request, response) => {
-  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
-  // remove (delete) the element from the array
-  function findBook(x){
-    return x.title === request.body.title;
-  }
-  const index = fakeArr.findIndex(findBook);
-
-  fakeArr.splice(index, 1);
 });
 
 app.listen(5001, () => {
