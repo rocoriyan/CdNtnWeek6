7c6c300348f4a630ba45e43b609ee18f69367bbb
rocoriyan <rcorrigan.other+github@gmail.com>
2024-02-09 15:54:24 +0000

    add deleteAll, deleteASingleBook, addManyBooks

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -92,6 +92,11 @@ const searchAndUpdate = async(request, response) => { //this works but the "new 
 }
 
 const findByTitle = async(request, response) => {
+    /*request body format
+        {
+            "title": ""
+        }
+    */
     try{
         const book = await Book.findOne({title: request.body.title});
         await logTypeOfResult(book);
@@ -102,11 +107,99 @@ const findByTitle = async(request, response) => {
     }
 }
 
+const deleteAll = async(request, response) => { //tested and working
+    try{
+        const book = await Book.deleteMany();
+        await logTypeOfResult(book);
+        response.send({ message: "success deleted books", book: book });
+    }
+    catch(error){
+        response.send({ message: "an error has occured", error: error});
+    }
+}
+
+const deleteASingleBook = async(request, response) => { //tested and working
+    try{
+        const book = await Book.deleteOne();
+        await logTypeOfResult(book);
+        response.send({ message: "success deleted book", book: book });
+    }
+    catch(error){
+        response.send({ message: "an error has occured", error: error});
+    }
+}
+
+const addManyBooks = async(request, response) =>{ //does not work. breaks on "addedBook = await.Book.Create" line. i dont know why.
+    try{
+        //maybe like a foreach function? go through every object inside body object and call addBook using information specific to them
+        /*body will look something like:
+            {
+                "1": {
+                    "title": "To Catch a Pikachu",
+                    "author": "Team Rocket",
+                    "genre": "Gothic"
+                },
+                "2": {
+                    "title": "The Midnight SQLi",
+                    "author": "A.U. Thor",
+                    "genre": "Thriller"
+                },
+                "3": {
+                    "title": "Trailing Commas",
+                    "author": "J. Sonne",
+                    "genre": "Young Adult"
+                },
+                "4": {
+                    "title": "Duck-tective V3",
+                    "author": "Stan A. Hirsch",
+                    "genre": "Mystery"
+                }
+            }
+        */
+        //key names for each book dont matter
+        let addedBook = {};
+        let addedBookList = [];
+        for(var book in request.body){
+            console.log((request.body)[book]);
+            addedBook = await Book.create({
+                "title": (request.body)[book].title, //string = object.object.key's value
+                "author": (request.body)[book].author,
+                "genre": (request.body)[book].genre,
+            });
+            console.log("i just added a book !");
+            addedBookList.push(addedBook);
+        };
+        await logTypeOfResult(book);
+        response.send({ message: "success added books", books: addedBookList});
+    }
+    catch(error){
+        response.send({ message: "an error has occured", error: error});
+    }
+    /*
+    {
+        "message": "an error has occured",
+        "error": {
+            "index": 0,
+            "code": 11000,
+            "keyPattern": {
+            "title": 1
+            },
+            "keyValue": {
+            "title": "To Catch a Pikachu"
+            }
+        }
+    }
+    */
+}
+
 module.exports = { //obj holding all functions to be exported
     addBook: addBook,
     getAllBooks: getAllBooks,
     updateBook: updateBook,
     deleteBook: deleteBook,
     searchAndUpdate: searchAndUpdate,
-    findByTitle: findByTitle
+    findByTitle: findByTitle,
+    deleteAll: deleteAll,
+    deleteASingleBook: deleteASingleBook,
+    addManyBooks: addManyBooks
 };
--- src/books/myRoutes.js
+++ src/books/myRoutes.js
@@ -12,12 +12,14 @@ bookRouter.put("/books/updateBookAuthor", controllers.updateBook); //search by b
 
 bookRouter.put("/books/searchAndUpdate", controllers.searchAndUpdate); //search by book title with route query and update any field using request body
 
-bookRouter.delete("/books/deleteOneBook", /*delete last book added function*/);
+bookRouter.delete("/books/deleteOneBook", controllers.deleteASingleBook); //just deletes one book
 
-bookRouter.delete("/books/deleteAllBooks", /*delete all function*/)
+bookRouter.delete("/books/deleteAllBooks", controllers.deleteAll); //deleted all books
 
-bookRouter.delete("/books/deleteBookByTitle", controllers.deleteBook);
+bookRouter.delete("/books/deleteBookByTitle", controllers.deleteBook); //deletes a book based on title in request body
 
-bookRouter.get("/books/findByTitle", controllers.findByTitle);
+bookRouter.get("/books/findByTitle", controllers.findByTitle); //finds book based on title in request body
+
+bookRouter.post("/books/addManyBooks", controllers.addManyBooks); //adds many books
 
 module.exports = bookRouter;
